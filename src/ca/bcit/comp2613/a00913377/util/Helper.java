c282af5d9180d99596746d48fdfd39094ce590d2
A00913377 <A00913377@DTC01L0890-14.ad.bcit.ca>
2014-05-21 20:12:00 -0700

    new packages

---
+++ src/ca/bcit/comp2613/a00913377/util/Helper.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ca.bcit.comp2613.a00913377.util;
+
+/**
+ * @author A00913377
+ *
+ */
+public class Helper {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/ca/bcit/comp2613/rockpaperscissorslizardspocksim/Driver.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ca.bcit.comp2613.rockpaperscissorslizardspocksim;
+
+/**
+ * @author A00913377
+ *
+ */
+public class Driver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

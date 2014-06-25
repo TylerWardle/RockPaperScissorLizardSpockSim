f5eac59ec895a8c7f68c3a1f900e50f383ee5209
TylerWardle <clapoutloud@gmail.com>
2014-06-24 22:57:15 -0700

    more work on PlayerBrowser adding functional buttons for CRUD

--- src/ca/bcit/comp2613/a00913377/util/PlayerBrowser.java
+++ src/ca/bcit/comp2613/a00913377/util/PlayerBrowser.java
@@ -7,29 +7,54 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.BoxLayout;
+
 import java.awt.List;
 import java.awt.Color;
+
 import javax.swing.border.TitledBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
 import java.awt.GridLayout;
+
 import javax.swing.JTextField;
+
 import java.awt.GridBagLayout;
+
 import javax.swing.JList;
+
 import java.awt.GridBagConstraints;
+
 import com.jgoodies.forms.layout.FormLayout;
 import com.jgoodies.forms.layout.ColumnSpec;
 import com.jgoodies.forms.layout.RowSpec;
 import com.jgoodies.forms.factories.FormFactory;
+
 import net.miginfocom.swing.MigLayout;
+
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JSeparator;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
 
-public class TestSwing extends JFrame {
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import ca.bcit.comp2613.rockpaperscissorslizardspocksim.model.Player;
+
+import java.util.ArrayList;
+
+public class PlayerBrowser extends JFrame {
 	private JTextField playerName;
 	private JTextField roundsPlayed;
 	private JTextField roundsWon;
 	private JTextField roundsLost;
 	private JTextField roundsTied;
+	private JTable table;
+	private ArrayList<Player> players;
+	public String[] columnNames = new String[] { "Name", "Rounds Won", "Rounds Lost", "Rounds Tied" };
+	public PlayerBrowserModel playerBrowserModel;
 
 	/**
 	 * Launch the application.
@@ -38,7 +63,7 @@ public class TestSwing extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					TestSwing frame = new TestSwing();
+					PlayerBrowser frame = new PlayerBrowser();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -50,7 +75,56 @@ public class TestSwing extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public TestSwing() {
+	public PlayerBrowser() {
+		players = TestDriver.buildPlayersAndTeams();
+		initialize();
+	}
+	private void initTable() {
+
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table.getSelectionModel().addListSelectionListener(
+				new ListSelectionListener() {
+
+					@Override
+					public void valueChanged(ListSelectionEvent e) {
+						if (e.getValueIsAdjusting()) {
+							populateFields();
+						}
+					}
+				});
+		refreshTable();
+
+	}
+	public void populateFields(){
+		try {
+			playerName.setText(table.getModel()
+					.getValueAt(table.getSelectedRow(), 0).toString());
+			roundsWon.setText(table.getModel()
+					.getValueAt(table.getSelectedRow(), 1).toString());
+			roundsLost.setText(table.getModel()
+					.getValueAt(table.getSelectedRow(), 2).toString());
+			roundsTied.setText(table.getModel()
+					.getValueAt(table.getSelectedRow(), 3).toString());			
+		} catch (Exception e) {}
+	}
+	
+	public void refreshTable(){
+		Object[][] data = null;
+
+		data = new Object[players.size()][4];
+		int i = 0;
+		for (Player player : players) {
+			data[i][0] = player.getName();
+			data[i][1] = player.getRoundsWon();
+			data[i][2] = player.getRoundsLost();
+			data[i][3] = player.getRoundsTied();
+			i++;
+		}
+		playerBrowserModel.setDataVector(data, columnNames);
+		table.repaint();
+	}
+	
+	public void initialize(){
 		setTitle("Player Browser");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 650, 500);
@@ -102,18 +176,38 @@ public class TestSwing extends JFrame {
 		getContentPane().add(lblTied);
 		
 		JButton btnCreate = new JButton("Create");
+		btnCreate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				doCreate();
+			}
+		});
 		btnCreate.setBounds(45, 318, 99, 25);
 		getContentPane().add(btnCreate);
 		
 		JButton btnList = new JButton("List");
+		btnList.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				doList();
+			}
+		});
 		btnList.setBounds(191, 318, 99, 25);
 		getContentPane().add(btnList);
 		
 		JButton btnUpdate = new JButton("Update");
+		btnUpdate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				doUpdate();
+			}
+		});
 		btnUpdate.setBounds(337, 318, 99, 25);
 		getContentPane().add(btnUpdate);
 		
 		JButton btnDelete = new JButton("Delete");
+		btnDelete.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				doDelete();
+			}
+		});
 		btnDelete.setBounds(483, 318, 99, 25);
 		getContentPane().add(btnDelete);
 		
@@ -121,9 +215,21 @@ public class TestSwing extends JFrame {
 		separator.setBounds(12, 356, 610, 7);
 		getContentPane().add(separator);
 		
-		List list = new List();
-		list.setMultipleSelections(false);
-		list.setBounds(10, 10, 612, 290);
-		getContentPane().add(list);
+		table = new JTable();
+		table.setBounds(12, 279, 608, -246);
+		getContentPane().add(table);
+	}
+	
+	public void doCreate(){
+		
+	}	
+	public void doList(){
+		
+	}
+	public void doUpdate(){
+		
+	}
+	public void doDelete(){
+		
 	}
 }

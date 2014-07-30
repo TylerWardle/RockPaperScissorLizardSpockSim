18cde2401238538b274ea5b08849bcc8f7d2954f
twardle <twardle@twardle.day4energy.local>
2014-07-29 23:15:31 -0700

    cleaning and figuring out how to set up the pom for a proper build

--- src/ca/bcit/comp2613/coursematerial/wow/DontHaveTheExpansionException.java
+++
@@ -1,9 +0,0 @@
-package ca.bcit.comp2613.coursematerial.wow;
-
-public class DontHaveTheExpansionException extends RuntimeException{
-
-	public String toString(){
-		return "Don't have the expansion for PANDAREN race";
-	}
-
-}
